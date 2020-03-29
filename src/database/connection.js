f7d31d0a15a3980a76af8a8c8ff649db837cf9c3
p21wilson <dev261456>
2020-03-29 02:11:30 -0300

    banco config

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,16 +1,9 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-
-
 // pegando uma variavel de ambiente 
 const config = process.env.NODE_ENV === 'test'  ? configuration.test : configuration.development;
 
-
 const connection = knex(config);
-
-beforeEach( async () => {
-     await connection.migrate.latest;
-});
 
 module.exports = connection;
